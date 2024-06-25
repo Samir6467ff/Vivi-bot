677e7a0ca9db8aa6299a20a1f4b06751bad4ef35
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-25 03:08:07 +0300

    Update gc-group.js

--- plugins/gc-config.js
+++ plugins/gc-config.js
@@ -1,33 +1,30 @@
-
 const handler = async (m, {conn, args, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.gc_config
 
   const isClose = { // Switch Case Like :v
-    'open': 'not_announcement',
-    'close': 'announcement',
+    'فتح': 'not_announcement',
+    'قفل': 'announcement',
     'abierto': 'not_announcement',
     'cerrado': 'announcement',
     'abrir': 'not_announcement',
     'cerrar': 'announcement',
   }[(args[0] || '')];
   if (isClose === undefined) {
-    throw `
-${tradutor.texto1[0]}
-
-${tradutor.texto1[1]}
-*┠┉↯ ${usedPrefix + command} abrir*
-*┠┉↯ ${usedPrefix + command} cerrar*
-`.trim();
+    throw `*[❗] تنسيق خاطئ!!*
+╮═━─━═⬣ *مثال* ⬣═━─━═╭
+⸙↜ .جروب فتح
+⸙↜ .جروب قفل
+╯═━──━═⬣❈⬣═━──━═╰`.trim();
   }
   await conn.groupSettingUpdate(m.chat, isClose);
-  {m.reply(`${tradutor.texto1[0]}`);}
+  {m.reply(`*❐┃ تم تنفيذ العملية بنجاح ┃✅ ❯*`);}
 };
 handler.help = ['group open / close', 'grupo abrir / cerrar'];
 handler.tags = ['group'];
-handler.command = /^(group|grupo)$/i;
+handler.command = /^(جروب)$/i;
 handler.admin = true;
 handler.botAdmin = true;
 export default handler;
