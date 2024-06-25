9cbdd071b99e789da27e51719d7ad49e4c2384ea
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-25 03:53:39 +0300

    Update gc-لينك.js

--- plugins/gc-لينك.js
+++ plugins/gc-لينك.js
@@ -3,13 +3,12 @@ import fs from 'fs';
 const handler = async (m, {conn, args}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.gc_link
 
   const group = m.chat;
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
-    contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-      title: tradutor.texto1[0]
+    contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null
     };
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
