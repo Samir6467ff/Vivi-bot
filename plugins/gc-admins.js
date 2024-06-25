5c761a2dbac97a36e54f6e6f6c613b0814d00c73
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-25 03:05:40 +0300

    Update gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -1,10 +1,9 @@
 
 
-
 const handler = async (m, {conn, participants, groupMetadata, args}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.gc_admins
 
   const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/admins.jpg';
@@ -13,14 +12,14 @@ const handler = async (m, {conn, participants, groupMetadata, args}) => {
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
   const pesan = args.join` `;
   const oi = `${tradutor.texto1[3]} ${pesan}`;
-  const text = `${tradutor.texto1[0]}
+  const text = `╮═━═⬣استدعاء المشرفين⬣═━═╭
+ال${oi}
 
-${oi}
-
-${tradutor.texto1[1]}
+*${tradutor.texto1[1]}*
 ${listAdmin}
 
-${tradutor.texto1[2]}`.trim();
+*[⚠️] استخدام هاذا الامر لا يكون الا لضرورة ، غير ذلك قد تأخذ انذار وقد يصل لطرد.*
+╯═━───━═⬣❈⬣═━───━═╰`.trim();
   conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
 };
 handler.help = ['admins <texto>'];
